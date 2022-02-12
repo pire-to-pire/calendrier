8d9f200c1d5fb1771d8b5fb08129cd9eee28736e
ango2 <pub.toine@gmail.com>
2022-02-12 20:53:56 +0100

    Calendrier complexe a partir des colonnes

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 X = 0
-compteurdecolonnes=0
+compteurdecolonnes = 0
 taille = 31
-debut=6
-#debut=(debut-1)
+debut = 6
+compteurdecolonnes = debut
+# debut=(debut-1)
 print("Lu Ma Me Je Ve Sa Di")
-for vides in range (0,debut):
+for vides in range(0, debut):
     print(end="   ")
-for semaine in range(1, (taille + 6) // 7 + 1):
-    for jours in range(X + 1, X + 8):
-        string_jours = str(jours) + " "
-        string_jours = string_jours.zfill(3)
-        if jours > taille:
-            string_jours = ""
-        print(string_jours, end="")
-    print("")
-    X = jours
+for loop in range(1,taille+1):
+    print(str(loop).zfill(2), end=" ")
+    compteurdecolonnes=compteurdecolonnes+1
+    if compteurdecolonnes==7:
+        compteurdecolonnes=0
+        print("")
+print("")
 print("Lu Ma Me Je Ve Sa Di")
